512a2b3ab02d5151f7ac70ced992fa32cb5cdd04
Fernando Serrano <ferchoserrano0@gmail.com>
2024-10-14 18:05:06 -0600

    2. Agregar la opción para poder agregar nuevas tareas al listado, cada una con su título y duración en minutos.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,17 +8,28 @@ import { Tarea } from './tarea';
 	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
 	tareas: Tarea[];
+	nuevaTarea: Tarea = new Tarea(0, '', 0); // Objeto para almacenar nueva tarea
 
 	constructor(
-        public service: AppService,
+		public service: AppService,
 	) { }
-	
+
 	ngOnInit() {
 		this.obtenerTareas();
 	}
 
 	async obtenerTareas() {
 		this.tareas = await this.service.obtenerTareas();
+	}
+
+
+	agregarTarea() {
+		if (this.nuevaTarea.titulo && this.nuevaTarea.minutos) {
+			this.nuevaTarea.id = this.tareas.length + 1; // Asignar un nuevo ID
+			this.tareas.push({ ...this.nuevaTarea, seleccionada: false, destacada: false });
+			this.nuevaTarea = new Tarea(0, '', 0); // Reiniciar el formulario
+		}
 	}
 }
--- src/app/tarea.ts
+++ src/app/tarea.ts
@@ -1,7 +1,11 @@
 export class Tarea {
+
+    public seleccionada: boolean = false; // Para controlar si la tarea está seleccionada
+    public destacada: boolean = false; // Para controlar si la tarea está destacada
+
     constructor(
         public id: number,
         public titulo: string,
         public minutos: number,
-    ){}
+    ) { }
 }
