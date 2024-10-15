070f0aeee7dbff87d5f308e513e3bb710cd08a68
Fernando Serrano <ferchoserrano0@gmail.com>
2024-10-14 18:09:16 -0600

    5. Agregar la opción de poder ordenar de manera ascendente o descendente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,7 @@ export class AppComponent {
 
 	tareas: Tarea[];
 	nuevaTarea: Tarea = new Tarea(0, '', 0); // Objeto para almacenar nueva tarea
+	ordenAscendente: boolean = true; // Para controlar el orden
 
 	constructor(
 		public service: AppService,
@@ -24,7 +25,7 @@ export class AppComponent {
 		this.tareas = await this.service.obtenerTareas();
 	}
 
-
+	// Método para agregar una nueva tarea
 	agregarTarea() {
 		if (this.nuevaTarea.titulo && this.nuevaTarea.minutos) {
 			this.nuevaTarea.id = this.tareas.length + 1; // Asignar un nuevo ID
@@ -34,7 +35,15 @@ export class AppComponent {
 	}
 
 
+	// Método para eliminar una tarea
 	eliminarTareas() {
         this.tareas = this.tareas.filter(tarea => !tarea.seleccionada);
+    }
+
+
+	// Método para cambiar el estado de una tarea
+	ordenar(propiedad: string) {
+        this.ordenAscendente = !this.ordenAscendente; // Cambiar el orden
+        this.tareas.sort((a, b) => this.ordenAscendente ? (a[propiedad] > b[propiedad] ? 1 : -1) : (a[propiedad] < b[propiedad] ? 1 : -1));
     }
 }
